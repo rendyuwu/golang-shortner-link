24a0313e5f20e3b51ff69e2a932aa5d0067a8858
Rendy Wijaya <rendy@biznetgio.com>
2022-10-04 08:37:53 +0000

    add router

---
+++ exception/error_handler.go
@@ -0,0 +1,98 @@
+package exception
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/rendyuwu/golang-shortner-link/helper"
+	"github.com/rendyuwu/golang-shortner-link/model/web"
+)
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
+
+	if notFoundError(w, r, err) {
+		return
+	}
+
+	if alreadyExistError(w, r, err) {
+		return
+	}
+
+	if validationError(w, r, err) {
+		return
+	}
+
+	internalServerError(w, r, err)
+}
+
+func validationError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func notFoundError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(NotFoundError)
+	if ok {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "NOT FOUND",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+
+		return true
+	} else {
+		return false
+	}
+}
+
+func alreadyExistError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(AlreadyExistError)
+	if ok {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusConflict,
+			Status: "CUSTOM CODE ALREADY EXIST",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+
+		return true
+	} else {
+		return false
+	}
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request, err interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+
+	webResponse := web.WebResponse{
+		Code:   http.StatusInternalServerError,
+		Status: "INTERNAL SERVER ERROR",
+		Data:   err,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
---
+++ router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/rendyuwu/golang-shortner-link/controller"
+	"github.com/rendyuwu/golang-shortner-link/exception"
+)
+
+func NewRouter(tokenController controller.TokenController, shortnerController controller.ShortnerController) *httprouter.Router {
+	router := httprouter.New()
+
+	// route token
+	router.POST("/api/v1/token", tokenController.Create)
+
+	// route shortner
+	router.GET("/:code", shortnerController.Find)
+	router.GET("/api/v1/shortner/:code", shortnerController.FindByCode)
+	router.POST("/api/v1/shortner", shortnerController.Create)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	return router
+}
