52dd67af19646aa235f45a549c45571a4addd766
Rendy Wijaya <rendy@biznetgio.com>
2022-10-04 08:58:39 +0000

    add router and middleware

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rendyuwu/golang-shortner-link/controller"
 	"github.com/rendyuwu/golang-shortner-link/env"
 	"github.com/rendyuwu/golang-shortner-link/helper"
+	"github.com/rendyuwu/golang-shortner-link/middleware"
 	"github.com/rendyuwu/golang-shortner-link/repository"
 	"github.com/rendyuwu/golang-shortner-link/router"
 	"github.com/rendyuwu/golang-shortner-link/service"
@@ -30,5 +31,11 @@ func main() {
 
 	router := router.NewRouter(tokenController, shortnerController)
 
-	server := http.Server{}
+	server := http.Server{
+		Addr:    "0.0.0.0:8888",
+		Handler: middleware.NewAuthMiddleware(router, tokenService),
+	}
+
+	err = server.ListenAndServe()
+	helper.PanicIfError(err)
 }
